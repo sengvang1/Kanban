2c2c12fe9fbfff42a60bcd9a959bad9ac3f7a375
sengvang1 <79110702+sengvang1@users.noreply.github.com>
2023-12-01 16:58:04 -0600

    Done

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -6,6 +6,8 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.models';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -14,6 +16,35 @@ import {
 })
 export class MainViewComponent {
 
+  board: Board = new Board('Test Board', [
+    new Column ("To-Dos", [
+      "Eat",
+      "Grind",
+      "Sleep",
+      "Repeat"
+    ]),
+    new Column ("Done", [
+      "Gym",
+      "Dishes",
+      "Clean",
+      "Running"
+    ]),
+    new Column ("Dream", [
+      "New Car",
+      "New House",
+      "New Boat",
+      "New Shoes",
+      "Race Car",
+      "Corvette"
+    ]),
+    new Column ("Action Items", [
+      "Dentist",
+      "Open enrollment",
+      "Taxes",
+      "Christmas Shopping",
+      "Letter to Santa"
+    ])
+  ]);
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
